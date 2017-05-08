f69147223d91f33c7ab684088b30fd659785e78f
Kemel Zaidan <kemelzaidan@gmail.com>
2017-05-08 00:51:48 -0300

    working but with loading problems

--- client/main.js
+++ client/main.js
@@ -3,24 +3,74 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+var colorSet = ['red','yellow','blue','green','orange','turquoise','beige','fuchsia','purple'];
+var colorNames = ['VERMELHO','AMARELO','AZUL','VERDE','LARANJA','TURQUESA','BEGE','ROSA','ROXO'];
+var limit = colorSet.length;
+
+// var circle1 = new ReactiveDict();
+// var circle2 = new ReactiveDict();
+// var circle3 = new ReactiveDict();
+// var circle4 = new ReactiveDict();
+
+// helper functions
+function getRandomPosition() {
+  return Math.floor(Math.random() * (limit + 1));
+}
+
+function getRightColor() {
+  let position = getRandomPosition();
+  let rightColor = {
+    color: colorSet[position],
+    name: colorNames[position],
+    attr: 'right',
+  };
+  return rightColor;
+}
+
+function getWrongColor() {
+  let position1 = getRandomPosition();
+  let position2 = getRandomPosition();
+  while (position1 === position2) {
+    position2 = getRandomPosition();
+  }
+  let wrongColor = {
+    color: colorSet[position1],
+    name: colorNames[position2],
+    attr: 'wrong',
+  };
+  return wrongColor;
+}
+
+function make4Circles() {
+  let circles = [];
+  circles.push(getRightColor());
+
+  for (let i = 0; i < 3; i++) {
+    circles.push(getWrongColor());
+  }
+  return circles;
+}
+
 // Template.hello.onCreated(function helloOnCreated() {
 //   // counter starts at 0
 //   this.counter = new ReactiveVar(0);
 // });
 
+Template.gameArea.helpers({
+  circles: () => {
+    return make4Circles();
+  }
+});
+
 Template.circle.helpers({
-  color() {
-    return 'red';
-  },
-
-  bcolor() {
-    return 'black';
-  },
+  circle: (circle) => {
+    return circle;
+  }
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
