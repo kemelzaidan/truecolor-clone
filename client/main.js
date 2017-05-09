bebef7729d4817aa01a25eefe80a8b40e1bc2325
Kemel Zaidan <kemelzaidan@gmail.com>
2017-05-09 13:47:25 -0300

    minor fixes

--- client/main.js
+++ client/main.js
@@ -50,8 +50,7 @@ function make4Circles() {
   return shuffle(circles);
 }
 
-Template.gameArea.onCreated(function helloOnCreated() {
-  // counter starts at 0
+Template.gameArea.onCreated(function () {
   this.circleArray = new ReactiveVar(make4Circles());
 });
 
